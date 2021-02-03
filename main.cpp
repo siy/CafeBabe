01edb95f68771200d5887fe32db180ba3d70beb5
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2021-02-03 10:02:22 +0100

    Add time tracking; Add more types of AST nodes to optimization list

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <sstream>
 #include <set>
+#include <chrono>
 #include <peglib.h>
 
 std::function<void(size_t, size_t, const std::string &)> makeErrorLogger(std::string &errorText) {
@@ -35,46 +36,55 @@ struct lint_result {
     std::string astOptimized;
 };
 
-static std::set<std::string> filter_safe { "__",
-                                           "Semicolon",
-                                           "BlockStart",
-                                           "BlockEnd",
-                                           "LP",
-                                           "RP",
-                                           "SequenceSign"};
+static std::set<std::string> filter_safe{
+        "__",
+        "BlockEnd",
+        "BlockStart",
+        "LP",
+        "RP",
+        "Semicolon",
+        "SequenceSign"
+};
 
-static std::set<std::string> filter_if_empty { "OptionalEllipsis" };
+static std::set<std::string> filter_if_empty{"OptionalEllipsis"};
 
-static std::set<std::string> filter_unsafe { "Comma",
-                                             "BitOr",
-                                             "LT",
-                                             "GT",
-                                             "Eq",
-                                             "LSqB",
-                                             "RSqB",
-                                             "Arrow",
-                                             "Colon"};
+static std::set<std::string> filter_unsafe{
+        "Arrow",
+        "BitOr",
+        "Colon",
+        "Comma",
+        "Eq",
+        "GT",
+        "LSqB",
+        "LT",
+        "RSqB",
+};
 
-static std::set<std::string> unsafe_parents { "TypeArguments",
-                                              "ImportList",
-                                              "OrType",
-                                              "MethodExpression",
-                                              "Constant",
-                                              "Type",
-                                              "SingleParamLambda",
-                                              "MultiparamLambda",
-                                              "TypeNameList",
-                                              "OptionalEllipsis",
-                                              "ArrayConstruction",
-                                              "NamedRange",
-                                              "Comprehension",
-                                              "ComprPipeline",
-                                              "TupleConstruction",
-                                              "MatchCase",
-                                              "ArrayElements",
-                                              };
+static std::set<std::string> unsafe_parents{
+        "ArrayConstruction",
+        "ArrayElements",
+        "Comprehension",
+        "ComprPipeline",
+        "Constant",
+        "ImportList",
+        "MatchCase",
+        "MethodExpression",
+        "MultiparamLambda",
+        "NamedAssignment",
+        "NamedAssignmentList",
+        "NamedRange",
+        "OptionalEllipsis",
+        "OrType",
+        "ParallelAssignment",
+        "SingleParamLambda",
+        "TupleConstruction",
+        "Type",
+        "TypeArguments",
+        "TypeNameList",
+        "ValueReference"
+};
 
-template <typename T>
+template<typename T>
 std::shared_ptr<T> optimize(bool optimize_unsafe,
                             std::shared_ptr<T> original,
                             std::shared_ptr<T> parent = nullptr) {
@@ -102,19 +112,45 @@ std::shared_ptr<T> optimize(bool optimize_unsafe,
     return ast;
 }
 
-bool lint(const std::string &grammarText, const std::string &codeText, std::string &fileName, lint_result& result) {
+bool lint(const std::string &grammarText, 
+          const std::string &codeText, 
+          std::string &fileName, 
+          lint_result &result, 
+          bool verbose) {
     std::string grammarErrors;
     std::string codeErrors = fileName + "\n";
     std::string astOptimizedText;
 
     peg::parser peg;
+
+    auto p0 = std::chrono::high_resolution_clock::now();
     auto ret = parse_grammar(grammarText, peg, grammarErrors);
+    auto p1 = std::chrono::high_resolution_clock::now();
+
+    if (verbose) {
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(p1 - p0);
+        std::cout << "parse grammar: " << duration.count() << "us" << std::endl;
+    }
 
     if (ret && peg) {
         std::shared_ptr<peg::Ast> ast;
         ret = parse_code(codeText, peg, codeErrors, ast);
+        auto p2 = std::chrono::high_resolution_clock::now();
+
+        if (verbose) {
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(p2 - p1);
+            std::cout << "parse code: " << duration.count() << "us" << std::endl;
+        }
+
         if (ast) {
             auto optimized = optimize(true, peg.optimize_ast(ast));
+            auto p3 = std::chrono::high_resolution_clock::now();
+
+            if (verbose) {
+                auto duration = std::chrono::duration_cast<std::chrono::microseconds>(p3 - p2);
+                std::cout << "optimize AST: " << duration.count() << "us" << std::endl;
+            }
+
             astOptimizedText = peg::ast_to_s(optimized);
         }
     }
@@ -146,7 +182,6 @@ int main(int argc, const char **argv) {
     }
 
     std::string grammar(grammarText);
-
     std::string fileName = argv[pos];
     std::ifstream ifs2(fileName);
 
@@ -164,17 +199,20 @@ int main(int argc, const char **argv) {
         std::cout << "Code:" << std::endl << code << std::endl;
     }
 
-    auto rc = lint(grammar, code, fileName, result);
+    auto start = std::chrono::high_resolution_clock::now();
+    auto rc = lint(grammar, code, fileName, result, verbose);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
     if (verbose) {
         if (rc) {
-            std::cout << "AST:"  << std::endl << result.astOptimized << std::endl;
+            std::cout << "AST:" << std::endl << result.astOptimized << std::endl;
         } else {
             std::cout << "Errors: " << std::endl << result.codeErrors << std::endl;
         }
     }
 
-    std::cout << fileName << (rc ? " PASS" : " FAIL") << std::endl;
+    std::cout << fileName << (rc ? " PASS" : " FAIL") << " in " << duration.count() << "ms" << std::endl;
 
     return rc ? 0 : -1;
 }
